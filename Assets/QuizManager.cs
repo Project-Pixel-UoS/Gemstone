0800161db7901415087b6fda37bae0a4eed1a4e8
Michael E <93289194+Doc-Steel@users.noreply.github.com>
2025-01-19 01:41:55 +0000

    [docs] Add some function summaries for QuizManager.cs.

--- Assets/QuizManager.cs
+++ Assets/QuizManager.cs
@@ -18,12 +18,20 @@ public class QuizManager : MonoBehaviour
         generateQuestion();
     }
 
+    /// <summary>
+    /// Called when the correct answer is provided by the player.  
+    /// Removes the current question from the list and generates a new question.
+    /// </summary>
     public void correctAnswerProvided()
     {
         QnA.RemoveAt(currentQuestionID);
         generateQuestion();
     }
 
+    /// <summary>
+    /// Sets the answer options for the current question.  
+    /// Updates the text and assigns whether each option is correct or not.
+    /// </summary>
     void SetAnswers()
     {
         for (int i = 0; i < options.Length; i++)
@@ -31,13 +39,17 @@ public class QuizManager : MonoBehaviour
             options[i].GetComponent<AnswerProcedure>().isCorrect = false;
             options[i].transform.GetChild(0).GetComponent<TextMeshPro>().text = QnA[currentQuestionID].Answers[i];
 
-            if (QnA[currentQuestionID].CorrectAnswerIndex == i+1)
+            if (QnA[currentQuestionID].CorrectAnswerIndex == i + 1)
             {
                 options[i].GetComponent<AnswerProcedure>().isCorrect = true;
             }
         }
     }
 
+    /// <summary>
+    /// Randomly generates a new question from the list and displays it.  
+    /// Removes the selected question from the list after it is displayed.
+    /// </summary>
     void generateQuestion()
     {
         currentQuestionID = Random.Range(0, QnA.Count);
@@ -47,6 +59,6 @@ public class QuizManager : MonoBehaviour
         SetAnswers();
 
         QnA.RemoveAt(currentQuestionID);
-
     }
+
 }
