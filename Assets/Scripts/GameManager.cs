587ba6244fb751acaff23451c229d3e2586c871d
Oskar Szymanek <oskarszymanek2004@gmail.com>
2025-02-16 12:13:22 +0000

    [refactor] updated how room switching works

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,17 +3,12 @@ using Util;
 using UnityEngine.SceneManagement;
 using Microsoft.Unity.VisualStudio.Editor;
 using JetBrains.Annotations;
+using UnityEditor.Animations;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-
-    [SerializeField] private GameObject cafe;
-    [SerializeField] private GameObject table;
-    [SerializeField] private GameObject store;
-    [SerializeField] private GameObject mainHall;
-    [SerializeField] private GameObject sign;
-    [SerializeField] private GameObject elevator;
-    [SerializeField] private GameObject backButton;
+    //references to all GameObjects
+    private GameObject panel, mainHall, cafe, table, store, reception, corridor1, backButton, backButton2;
 
     private void Awake()
     {
@@ -30,12 +25,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        //this will have to change according to starting location or prev. saved position
-        mainHall.SetActive(true);
-        backButton.SetActive(false);
-        cafe.SetActive(false);
-        table.SetActive(false);
-        store.SetActive(false);
+        InitialiseScene();
     }
 
     private void Update()
@@ -44,106 +34,109 @@ public class GameManager : MonoBehaviour
         {
             GetClickedScene();
         }
-
     }
 
-    //select which scene to switch to depending on which collider mouse collides w/
+    /// <summary>
+    /// Finds all the GameObjects in ground floor and caches them in their 
+    /// respective variable, this is done to ensure that the objects can be 
+    /// referenced when switching from first floor to ground floor.
+    /// </summary>
+    private void CacheGameObjects()
+    {
+        panel = GameObject.FindWithTag("GameStage");
+        if (panel == null) return;
+
+        mainHall = panel.transform.Find("Room: Main Hall")?.gameObject;
+        cafe = panel.transform.Find("Room: Cafe")?.gameObject;
+        table = panel.transform.Find("Room: Table")?.gameObject;
+        store = panel.transform.Find("Room: Shop Front")?.gameObject;
+        backButton = panel.transform.Find("BackButton")?.gameObject;
+        reception = panel.transform.Find("Room: Reception")?.gameObject;
+        corridor1 = panel.transform.Find("Room: Corridor 1")?.gameObject;
+        backButton2 = panel.transform.Find("BackButton2")?.gameObject;
+
+    }
+    
+    /// <summary>
+    /// Intialises the ground floor scene
+    /// </summary>
+    private void InitialiseScene()
+    {
+        if (mainHall == null || backButton == null) return;
+
+        mainHall.SetActive(true);
+        backButton.SetActive(false);
+        cafe?.SetActive(false);
+        table?.SetActive(false);
+        store?.SetActive(false);
+    }
+
     private void GetClickedScene()
     {
+        CacheGameObjects();
         var clickedItem = Utils.CalculateMouseDownRaycast(LayerMask.GetMask("Default")).collider;
         if (clickedItem == null) return;
+
         switch (clickedItem.gameObject.name)
         {
-            case "Store":
-                OnStoreClicked();
-                break;
-            case "Chair":
-                OnChairClicked();
-                break;
-            case "Sign":
-                OnSignClicked();
-                break;
-            case "Elevator":
-                OnElevatorClicked();
-                break;
-            case "BackButton":
-                OnBackButtonClicked();
-                break;
-            case "BackButton2":
-                OnBackButton2Clicked();
-                break;
-            case "Corridor 1":
-                OnCorridor1Clicked();
-                break;
-            default:
-                break;
-        }
-    }
-    public void OnSignClicked()
-    {
-        mainHall.SetActive(false);
-        cafe.SetActive(true);
-        backButton.SetActive(true);
-    }
-    public void OnChairClicked()
-    {
-        cafe.SetActive(false);
-        table.SetActive(true);
-    }
-    public void OnStoreClicked()
-    {
-        cafe.SetActive(false);
-        store.SetActive(true);
-    }
-    public void OnElevatorClicked()
-    {
-        SceneManager.LoadScene("First Floor");
-    }
-    public void OnCorridor1Clicked()
-    {
-        GameObject panel = GameObject.FindWithTag("GameStage");
-        //transform.Find instead of gameObject.find so you can find inactive gameObjects
-        GameObject reception = panel.transform.Find("Room: Reception").gameObject;
-        GameObject corridor1Room = panel.transform.Find("Room: Corridor 1").gameObject;
-        reception.SetActive(false);
-        corridor1Room.SetActive(true);
-    }
-    public void OnBackButtonClicked()
-    {
-        //note: need to make our own back button graphic to avoid copyright lol
-        if (table.activeSelf == true || store.activeSelf == true)
-        {
-            cafe.SetActive(true);
-            table.SetActive(false);
-            store.SetActive(false);
-        }
-        else
-        {
-            mainHall.SetActive(true);
-            cafe.SetActive(false);
-            backButton.SetActive(false);
+            case "Store": OnStoreClicked(); break;
+            case "Chair": OnChairClicked(); break;
+            case "Sign": OnSignClicked(); break;
+            case "Elevator": OnElevatorClicked(); break;
+            case "BackButton": OnBackButtonClicked(); break;
+            case "BackButton2": OnBackButton2Clicked(); break;
+            case "Corridor 1": OnCorridor1Clicked(); break;
         }
     }
     /// <summary>
-    /// There must be a better way to do this but atm you don't have much choice
-    /// because the backbutton from the ground floor is deleted so you have to 
-    /// reference a new one, but this seems bad for when we have more scenes.
+    /// Loops through each child of the panel transform and deactivates them,
+    /// then activates only the room that you want to show.
     /// </summary>
+    /// <param name="roomToShow"></param>
+    private void SwitchRooms(GameObject roomToShow)
+    {
+        if (panel == null || roomToShow == null) return;
+
+        foreach (Transform child in panel.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+        roomToShow.SetActive(true);
+        if (roomToShow != mainHall) backButton?.SetActive(true);
+    }
+    /// <summary>
+    /// Room switching functions
+    /// </summary>
+    public void OnSignClicked() => SwitchRooms(cafe);
+    public void OnChairClicked() => SwitchRooms(table);
+    public void OnStoreClicked() => SwitchRooms(store);
+    public void OnCorridor1Clicked() => SwitchRooms(corridor1);    
+    public void OnElevatorClicked() => SceneManager.LoadScene("First Floor");
+
+    public void OnBackButtonClicked()
+    {
+        //note: need to make our own back button graphic to avoid copyright
+        if (table?.activeSelf == true || store?.activeSelf == true)
+        {
+            SwitchRooms(cafe);
+        }
+        else
+        {
+            SwitchRooms(mainHall);
+            backButton?.SetActive(false);
+        }
+    }
+
     public void OnBackButton2Clicked()
     {
-        GameObject panel = GameObject.FindWithTag("GameStage");
-        GameObject reception = panel.transform.Find("Room: Reception").gameObject;
-        GameObject corridor1Room = panel.transform.Find("Room: Corridor 1").gameObject;
-        if (reception.activeSelf == true)
+        if (reception?.activeSelf == true)
         {
-            //bad bad bad, no objects in first floor so pointless atm
             SceneManager.LoadScene("Ground Floor");
         }
         else
-        //corridor 1 active condition, will add more when corridor 2 is a thing
         {
-            corridor1Room.SetActive(false);
-            reception.SetActive(true);
+            SwitchRooms(reception);
         }
     }
 }
