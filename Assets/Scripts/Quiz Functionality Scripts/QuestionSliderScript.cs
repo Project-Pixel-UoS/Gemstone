f2ff89c8a58569d4d217b9a9b67d7194c4f7145c
Michael E <93289194+Doc-Steel@users.noreply.github.com>
2025-02-23 11:07:08 +0000

    [wip] Add listener to slider script that updates text when slider is changed

--- Assets/Scripts/Quiz Functionality Scripts/QuestionSliderScript.cs
+++ Assets/Scripts/Quiz Functionality Scripts/QuestionSliderScript.cs
@@ -1,11 +1,20 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestionSliderScript : MonoBehaviour
 {
+
+    [SerializeField] private Slider slider;
+    [SerializeField] private TextMeshProUGUI sliderText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        slider.onValueChanged.AddListener((v) =>
+        {
+            sliderText.text = v.ToString("0.00");
+        }); 
     }
 
     // Update is called once per frame
