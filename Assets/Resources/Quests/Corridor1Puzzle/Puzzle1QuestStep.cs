99ae3e7b1d106b5ca7177831e860f18e09d7310a
Oskar Szymanek <oskarszymanek2004@gmail.com>
2025-02-26 13:01:14 +0000

    [feat] added logic for puzzle 1

--- Assets/Resources/Quests/Corridor1Puzzle/Puzzle1QuestStep.cs
+++ Assets/Resources/Quests/Corridor1Puzzle/Puzzle1QuestStep.cs
@@ -1,27 +1,61 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Util;
 
 public class Puzzle1QuestStep : QuestStep
 {
+    private string clickedScene;
+    private string[] correctOrder = { "Step1", "Step2", "Step3", "Step4" };
+    private List<string> clickedOrder = new List<string>();
     private void Update()
     {
         if (Utils.IsMouseClicked() && Utils.CheckMousePosInsideStage("GameStage"))
         {
-            GetClickedScene();
+            clickedScene = GetClickedScene();
+            clickedOrder.Add(clickedScene);
+        }
+        if (clickedOrder.Count == 4)
+        {
+            if (CompareOrders(correctOrder, clickedOrder.ToArray()))
+            {
+                Debug.Log("Correct order, finsh quest!");
+                FinishQuestStep();
+            }
+            else
+            {
+                clickedOrder.Clear();
+                Debug.Log("Try again!");
+            }
         }
     }
+        
+    
+    private bool CompareOrders(string[] correct, string[] clicked)
+    {
+        int correctCount = 0;
+        for (int i = 0; i < correct.Length; i++)
+        {
+            if (correct[i] == clicked[i])
+            {
+                correctCount++;
+            }
+        }
+        if (correctCount == correct.Length)
+        {
+            return true;
+        }
+        return false;
+    }
 
-    private void GetClickedScene()
+    private string GetClickedScene()
     {
         var clickedItem = Utils.CalculateMouseDownRaycast(LayerMask.GetMask("Default")).collider;
-        if (clickedItem == null) return;
-        
-        switch(clickedItem.gameObject.name)
-        {
-            case "Step1": FinishQuestStep(); break;
-            case "Step2": FinishQuestStep(); break;
-            case "Step3": FinishQuestStep(); break;
-            case "Step4": FinishQuestStep(); break;
+        if (clickedItem == null) { return null; }
+        else { 
+            return clickedItem.gameObject.name;
         }
     }
 }
