ed0140a4a6e53a7b9b2aed53349f09762fe974af
Michael E <93289194+Doc-Steel@users.noreply.github.com>
2025-02-27 23:42:59 +0000

    [wip] Add images and Send Message Function to toggle quiz section

---
+++ Assets/QuizToggleLogic.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuizToggleLogic : MonoBehaviour
+{
+    [SerializeField] private TMP_Text textfield;
+    [SerializeField] private string message;
+
+    public void SendMessage(bool toggleValue)
+    {
+        if (toggleValue)
+        {
+            textfield.SetText(message);
+        } else
+        {
+            textfield.SetText(sourceText: "Nothing Set");
+        }
+    }
+
+}
