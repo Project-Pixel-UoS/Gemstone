522e285726a7a96e3f4efa1a88fe845be829de6b
Oskar Szymanek <oskarszymanek2004@gmail.com>
2025-03-04 15:23:44 +0000

    [refactor] corridor 1 now starts active to avoid auto-clicking step 1

--- Assets/Resources/Quests/Corridor1Puzzle/Puzzle1QuestStep.cs
+++ Assets/Resources/Quests/Corridor1Puzzle/Puzzle1QuestStep.cs
@@ -10,29 +10,32 @@ public class Puzzle1QuestStep : QuestStep
     private string clickedScene;
     private string[] correctOrder = { "Step1", "Step2", "Step3", "Step4" };
     private List<string> clickedOrder = new List<string>();
+    //private int retryCount = 0;
     private void Update()
     {
         if (Utils.IsMouseClicked() && Utils.CheckMousePosInsideStage("GameStage"))
-        {
+        { 
             clickedScene = GetClickedScene();
             clickedOrder.Add(clickedScene);
+            Debug.Log(clickedScene);
         }
         if (clickedOrder.Count == 4)
         {
             if (CompareOrders(correctOrder, clickedOrder.ToArray()))
             {
-                Debug.Log("Correct order, finsh quest!");
                 FinishQuestStep();
             }
             else
             {
                 clickedOrder.Clear();
+                //retryCount++;
                 Debug.Log("Try again!");
+
             }
         }
     }
-        
-    
+
+
     private bool CompareOrders(string[] correct, string[] clicked)
     {
         int correctCount = 0;
@@ -57,5 +60,17 @@ public class Puzzle1QuestStep : QuestStep
         else { 
             return clickedItem.gameObject.name;
         }
+    }
+    //todo - implement spooky guy closer each fail
+    private void moveSpookyCloser(int retries)
+    {
+        switch (retries)
+        {
+            case 1: break;
+            case 2: break;
+            case 3: break;
+
+        }
+
     }
 }
