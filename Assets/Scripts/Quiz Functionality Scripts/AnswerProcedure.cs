05984d2260eac8ab282681dea1a08ebbe070abbc
Michael E <93289194+Doc-Steel@users.noreply.github.com>
2025-03-13 22:39:33 +0000

    [refactor] Update comments for AnswerProcedure.cs

--- Assets/Scripts/Quiz Functionality Scripts/AnswerProcedure.cs
+++ Assets/Scripts/Quiz Functionality Scripts/AnswerProcedure.cs
@@ -1,14 +1,11 @@
 using UnityEngine;
 
 /// <summary>
-/// Handles the logic for answering quiz questions and interacting with the QuizManager.
+/// Handles the logic for answering quiz questions (only for multiple choice section) and interacting with the QuizManager.
 /// </summary>
 /// <remarks>
 /// Maintained by: Michael Edems-Eze
 /// </remarks>
-/// <todo>
-/// - Add transitions when all questions are completed (e.g., fade to nighttime).
-/// </todo>
 public class AnswerProcedure : MonoBehaviour
 {
     // Indicates whether this answer option is correct.
@@ -27,24 +24,23 @@ public class AnswerProcedure : MonoBehaviour
     /// Called when the user selects an answer option.  
     /// If the answer is correct, triggers the correct answer logic in QuizManager; otherwise, triggers the incorrect logic.
     /// </summary>
+    /// <remarks>
+    /// Maintained by: Michael Edems-Eze
+    /// </remarks>
     public void Answer()
     {
         // Check if the selected answer is correct and handle accordingly.
         if (isCorrect)
         {
-            Debug.Log("Correct");
 
             // Increment the number of answered questions.
             answeredQuestionsNum++;
-
-            Debug.Log(answeredQuestionsNum);
 
             quizManager.correctAnswerProvided();
             
         }
         else
         {
-            Debug.Log("Not Correct");
             quizManager.incorrectAnswerProvided(); 
         }                
     }
