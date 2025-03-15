e2ea2d945424e79f12418f9ceae026c0fa9c49c1
Michael E <93289194+Doc-Steel@users.noreply.github.com>
2025-03-15 16:49:23 +0000

    [wip] Allow script to either open or close menu with the same button

--- Assets/Scripts/LeanTween Animations/Inventory Tweening.cs
+++ Assets/Scripts/LeanTween Animations/Inventory Tweening.cs
@@ -5,6 +5,23 @@ public class InventoryTweening : MonoBehaviour
 
     public LeanTweenType easeType;
     public float closeDelay;
+    public bool isInventoryOpen = true;
+
+    public void ToggleInventory()
+    {
+        if (isInventoryOpen)
+        {
+            OnClose();
+        } else
+        {
+            OnOpen();
+        }
+    }
+
+    public void OnOpen()
+    {
+        LeanTween.moveX(gameObject, -15f, 1f).setDelay(closeDelay).setEase(easeType);
+    }
 
     public void OnClose()
     {
