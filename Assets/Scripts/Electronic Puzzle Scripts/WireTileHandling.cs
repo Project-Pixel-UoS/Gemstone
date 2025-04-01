ca2ae97ce30d059eb94aec531201dc9156bd0d3f
Michael E <93289194+Doc-Steel@users.noreply.github.com>
2025-04-01 19:22:12 +0100

    [debug] Fix issue where dragging would send item off screen and behind other elements in canvas

--- Assets/Scripts/Electronic Puzzle Scripts/WireTileHandling.cs
+++ Assets/Scripts/Electronic Puzzle Scripts/WireTileHandling.cs
@@ -3,19 +3,29 @@ using UnityEngine.EventSystems;
 
 public class WireTileHandling : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    Transform parentAfterDrag; //Holds parent of dragged item before dragging begins
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("Begin Dragging");
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling(); //Places dragged object in front of everything else
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("We're still dragging!");
-        transform.position = Input.mousePosition;
+
+        //These three lines make sure dragging and dropping actually works
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10f;
+        transform.position = Camera.main.ScreenToWorldPoint(mousePos);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("We finally stopped dragging...");
+        transform.SetParent(parentAfterDrag);
     }
 }
