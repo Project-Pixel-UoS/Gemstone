7b67752061b7e5c7d35a94c92080d260318a2392
Michael E <93289194+Doc-Steel@users.noreply.github.com>
2025-04-02 13:40:24 +0100

    [wip] Create 2D list of WireTileHandling scripts

--- Assets/CircuitPuzzleManager.cs
+++ Assets/CircuitPuzzleManager.cs
@@ -1,21 +1,49 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CircuitPuzzleManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int gridWidth, gridHeight; // Define the grid size
+    private WireTileHandling[,] wireGrid; // 2D array for storing tiles
+
     void Start()
     {
-        
+        wireGrid = new WireTileHandling[gridWidth, gridHeight]; // Initialize grid
+
+        WireTileHandling[] tiles = GetComponentsInChildren<WireTileHandling>(); // Only check children
+
+        int count = 0; // Track how many tiles have been assigned
+        for (int y = 0; y < gridHeight; y++)
+        {
+            for (int x = 0; x < gridWidth; x++)
+            {
+                if (count < tiles.Length) // Ensure we don't go out of bounds
+                {
+                    RegisterTile(tiles[count], x, y);
+                    count++;
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RegisterTile(WireTileHandling tile, int x, int y)
     {
-        
+        if (x >= 0 && x < gridWidth && y >= 0 && y < gridHeight)
+        {
+            wireGrid[x, y] = tile;
+        }
+        else
+        {
+            Debug.LogWarning($"Tile at ({x},{y}) is out of bounds!");
+        }
     }
 
-    private void PopulateGrid()
-    { 
-
+    public WireTileHandling GetTileAtPosition(int x, int y)
+    {
+        if (x >= 0 && x < gridWidth && y >= 0 && y < gridHeight)
+        {
+            return wireGrid[x, y];
+        }
+        return null;
     }
 }
