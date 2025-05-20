9d5a60bb9313a0148948fa0f67ed2442fb7a5ea6
Michael E <93289194+Doc-Steel@users.noreply.github.com>
2025-05-20 02:03:59 +0100

    [feat] Include final updates for main menu and lobby themes in appropriate scenes
    Also renamed audio folders to make more sense

--- Assets/StartMainMenuMusic.cs
+++ Assets/StartMainMenuMusic.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class StartMainMenuMusic : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public AudioClip mainMenuMusic;
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        if (mainMenuMusic != null && AudioManagement.instance != null)
+        {
+            AudioManagement.instance.PlayMusic(mainMenuMusic);
+        }
     }
 }
